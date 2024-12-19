fb1052476981f7ec62fe30eeb0fc8d3e6d96ec67
NLGRF <non.devtech@gmail.com>
2024-12-19 23:37:06 +0700

    day1

---
+++ rust/day1-2/target/debug/day1-2.d
@@ -0,0 +1,1 @@
+/Users/non-maxion/workspaces/learning/advent-of-code-2024/rust/day1-2/target/debug/day1-2: /Users/non-maxion/workspaces/learning/advent-of-code-2024/rust/day1-2/src/main.rs
---
+++ rust/day1-2/target/debug/deps/day1_2-542fcded536fdebc.d
@@ -0,0 +1,5 @@
+/Users/non-maxion/workspaces/learning/advent-of-code-2024/rust/day1-2/target/debug/deps/day1_2-542fcded536fdebc: src/main.rs
+
+/Users/non-maxion/workspaces/learning/advent-of-code-2024/rust/day1-2/target/debug/deps/day1_2-542fcded536fdebc.d: src/main.rs
+
+src/main.rs:
